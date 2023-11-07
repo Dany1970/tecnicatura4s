2598903d51d1746e4c9f466ea52dc5cd2d677417
Daniela Armijo <111472176+Dany1970@users.noreply.github.com>
2023-11-07 08:08:34 -0300

    Nuevo archivo en Node

---
+++ Node/01-Hola-Mundo/app2.js
@@ -0,0 +1,4 @@
+const saludar = (nombre) => {//Función de flecha
+    return `Saludos de ${nombre}`//Retorna un string
+}// esto es igual a 'Saludos' + nombre
+console.log(saludar("Victoria"));//Imprime el resultado de la función
